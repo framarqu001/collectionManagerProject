a0b83456da79e0db31a71b4d74d6c1395b094bd6
framarqu001 <119390184+framarqu001@users.noreply.github.com>
2024-02-04 12:45:26 -0500

    Genre enum class

--- src/albumcollection/Genre.java
+++ src/albumcollection/Genre.java
@@ -1,4 +1,19 @@
 package albumcollection;
 
-public class Genre {
+public enum Genre {
+    POP("Pop"),
+    COUNTRY("Country"),
+    CLASSICAL("Classical"),
+    JAZZ("Jazz"),
+    UNKNOWN("Unknown");
+
+    final String name;
+
+    Genre (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

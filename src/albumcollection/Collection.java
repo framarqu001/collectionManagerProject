6b35c1d3faeb26a3fd89fcb278cff3f78afa22e1
framarqu001 <119390184+framarqu001@users.noreply.github.com>
2024-02-04 21:17:25 -0500

    Set up collection class.

--- src/albumcollection/Collection.java
+++ src/albumcollection/Collection.java
@@ -1,8 +1,42 @@
 package albumcollection;
 
 public class Collection {
+    private static final int INITIAL_CAPACITY = 4;
+
     private Album[] albums;
     private int size;
+
+    private int find(Album album){
+        return 0; //placeholder
+    }
+
+    private void grow(){
+
+    }
+
+    public boolean contains(Album album){
+        return true; //place holder
+    }
+
+    public boolean remove(Album album){
+        return true; //place holder
+    }
+
+    public void rate(Album album, int rating){
+
+    }
+
+    public void printBydate(){
+
+    }
+
+    public void printByGenre(){
+
+    }
+
+    public void printByRating(){
+
+    }
 
 
 }
